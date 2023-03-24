b4140d29f33be37c6d9e696957d82f0ed806eaac
HeonSeok98 <109660331+HeonSeok98@users.noreply.github.com>
2023-03-24 21:30:54 +0900

    [Add]: 마우스 호버

--- src/routes/Test.js
+++ src/routes/Test.js
@@ -7,15 +7,14 @@ function Test() {
 
   return (
     <div className="container">
-        <div>나는 인스타에 자랑질 하는 색끼덜 이해가 안된다</div>
-        <ul>
+        <div className="answer">나는 인스타에 자랑질 하는 색끼덜 이해가 안된다</div>
+        <ol>
           <li>매우 그렇다</li>
           <li>그렇다</li>
           <li>별 생각없다</li>
           <li>아니다</li>
           <li>전혀 아니다</li>
-        </ul>
-        <div>다음</div>
+        </ol>
     </div>
   );
 }
