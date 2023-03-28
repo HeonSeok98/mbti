75bad181e64d3a8e697e7eb7dc1c005a88895b51
HeonSeok98 <109660331+HeonSeok98@users.noreply.github.com>
2023-03-28 17:46:40 +0900

    [Add]: content 생성

---
+++ src/contents/result.js
@@ -0,0 +1,86 @@
+
+const result = [
+    {
+        image: '',
+        name: 'ISFJ',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ISFP',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ISTJ',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ISTP',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'INFJ',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'INFP',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'INTJ',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'INTP',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ESFJ',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ESFP',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ESTJ',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ESTP',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ENFJ',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ENFP',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ENTJ',
+        message: ''
+    },
+    {
+        image: '',
+        name: 'ENTP',
+        message: ''
+    }
+];
+
+export default result;
+
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 function Start() {
 
   return (
-    <div className="container">
+    <div>
         <div className="title">당신의 MBTI를 알아봅시다</div>
         <Link to="/test" className="btn">검사하러 가기</Link>
     </div>
