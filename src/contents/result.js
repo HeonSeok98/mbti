3ed3c63742d7c98f99adc747c47e5245b762339e
HeonSeok98 <109660331+HeonSeok98@users.noreply.github.com>
2023-03-28 22:33:20 +0900

    [Fix]: 탭간격 수정

--- src/contents/result.js
+++ src/contents/result.js
@@ -1,85 +1,85 @@
 
 const result = [
-    {
-        image: 'isfj.png',
-        name: 'ISFJ',
-        message: ''
-    },
-    {
-        image: 'isfp.png',
-        name: 'ISFP',
-        message: ''
-    },
-    {
-        image: 'istj.png',
-        name: 'ISTJ',
-        message: ''
-    },
-    {
-        image: 'istp.png',
-        name: 'ISTP',
-        message: ''
-    },
-    {
-        image: 'infj.png',
-        name: 'INFJ',
-        message: ''
-    },
-    {
-        image: 'infp.png',
-        name: 'INFP',
-        message: ''
-    },
-    {
-        image: 'intj.png',
-        name: 'INTJ',
-        message: ''
-    },
-    {
-        image: 'intp.png',
-        name: 'INTP',
-        message: ''
-    },
-    {
-        image: 'esfj.png',
-        name: 'ESFJ',
-        message: ''
-    },
-    {
-        image: 'esfp.png',
-        name: 'ESFP',
-        message: ''
-    },
-    {
-        image: 'estj.png',
-        name: 'ESTJ',
-        message: ''
-    },
-    {
-        image: 'estp.png',
-        name: 'ESTP',
-        message: ''
-    },
-    {
-        image: 'enfj.png',
-        name: 'ENFJ',
-        message: ''
-    },
-    {
-        image: 'enfp.png',
-        name: 'ENFP',
-        message: ''
-    },
-    {
-        image: 'entj.png',
-        name: 'ENTJ',
-        message: ''
-    },
-    {
-        image: 'entp.png',
-        name: 'ENTP',
-        message: ''
-    }
+  {
+    image: 'isfj.png',
+    name: 'ISFJ',
+    message: ''
+  },
+  {
+    image: 'isfp.png',
+    name: 'ISFP',
+    message: ''
+  },
+  {
+    image: 'istj.png',
+    name: 'ISTJ',
+    message: ''
+  },
+  {
+    image: 'istp.png',
+    name: 'ISTP',
+    message: ''
+  },
+  {
+    image: 'infj.png',
+    name: 'INFJ',
+    message: ''
+  },
+  {
+    image: 'infp.png',
+    name: 'INFP',
+    message: ''
+  },
+  {
+    image: 'intj.png',
+    name: 'INTJ',
+    message: ''
+  },
+  {
+    image: 'intp.png',
+    name: 'INTP',
+    message: ''
+  },
+  {
+    image: 'esfj.png',
+    name: 'ESFJ',
+    message: ''
+  },
+  {
+    image: 'esfp.png',
+    name: 'ESFP',
+    message: ''
+  },
+  {
+    image: 'estj.png',
+    name: 'ESTJ',
+    message: ''
+  },
+  {
+    image: 'estp.png',
+    name: 'ESTP',
+    message: ''
+  },
+  {
+    image: 'enfj.png',
+    name: 'ENFJ',
+    message: ''
+  },
+  {
+    image: 'enfp.png',
+    name: 'ENFP',
+    message: ''
+  },
+  {
+    image: 'entj.png',
+    name: 'ENTJ',
+    message: ''
+  },
+  {
+    image: 'entp.png',
+    name: 'ENTP',
+    message: ''
+  }
 ];
 
 export default result;
